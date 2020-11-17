1d7025f6ab97a536ed4937f73d0a1a99a7f878f9
Wassim Khadraoui <wassim.khadraoui@ynov.com>
2020-11-17 13:02:48 +0100

    page home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,121 @@
 <template>
   <div>
-      <p>Home</p>
+     <section class="container">
+       <div class="head">
+       <img src="../assets/logo.png">
+       <a href="#">S'identifier</a>
+       </div>
+     </section>
+     <section class="banner">
+      <img src="../assets/back.jpg" class="fit">
+       <div class="box">
+        <div class="heading">
+          <h2>Films, séries TV et bien plus en illimité.</h2>
+          <h5>Où que vous soyez. Annulez à tout moment.</h5>
+          <p>Prêt à regarder Netflix ? Saisissez votre adresse e-mail 
+          pour vous abonner ou réactiver votre abonnement.</p>
+        </div>
+        <div class="search">
+        <input type="text" name="" placeholder="Enter Email">
+        <a> Rejoignez la communauté</a>
+        </div>
+       </div>
+</section>
+<section class="part1">
+<div class="talk">
+<h1>Regardez Netflix sur votre TV.</h1>
+<p>Regardez Netflix sur votre Smart TV, PlayStation, Xbox, Chromecast, 
+Apple TV, lecteurs Blu-ray et bien plus.</p>
+</div>
+<div class="img">
+<img src="../assets/part1.png">
+</div>
+</section>
+<section class="list">
+<h1>Foire aux questions</h1>
+<div class="accordion-menu">
+  <div class="item" id="item1">
+  <a href="#item1" class="title">
+  <span>Netflix, qu'est-ce que c'est ?</span>
+  <i class="icon"></i>
+  </a>
+  <p class="text">
+  Netflix est un service de streaming qui propose une vaste sélection de séries TV,
+   films, animes, documentaires et autres programmes primés sur des milliers d'appareils connectés à Internet.
+  </p>
   </div>
+   <div class="item" id="item2">
+  <a href="#item2" class="title">
+  <span>Combien coûte Netflix ??</span>
+  <i class="icon"></i>
+  </a>
+  <p class="text">
+  Regardez Netflix sur votre smartphone, tablette, Smart TV, 
+  
+  ordinateur ou appareil de streaming, le tout pour un tarif mensuel fixe. Les forfaits vont de 7,99 € à 
+  15,99 € par mois. Pas de contrat ni de frais supplémentaires.
+  </p>
+  </div>
+    <div class="item" id="item3">
+  <a href="#item3" class="title">
+  <span>Où puis-je regarder Netflix ?</span>
+  <i class="icon"></i>
+  </a>
+  <p class="text">
+Regardez où et quand vous voulez, sur un nombre illimité d'appareils. 
+Connectez-vous à votre compte pour regarder Netflix en ligne sur netflix.com depuis votre ordinateur ou tout appareil connecté à Internet avec l'application Netflix, comme les Smart TV, smartphones, tablettes, lecteurs de streaming et consoles de jeu.  </p>
+  </div>
+    <div class="item" id="item4">
+  <a href="#item4" class="title">
+  <span>Comment puis-je annuler mon forfait ?</span>
+  <i class="icon"></i>
+  </a>
+  <p class="text">
+Netflix offre une grande souplesse. Pas de contrat compliqué. Sans engagement. Deux clics suffisent pour annuler votre compte en ligne. Pas de frais d'annulation : ouvrez ou fermez votre compte à tout moment.  </p>
+  </div>
+</div>
+</section>
+<section class="banner2">
+<div class="box">
+        <div class="heading">
+         
+          <p>Prêt à regarder Netflix ? Saisissez votre adresse e-mail 
+          pour vous abonner ou réactiver votre abonnement.</p>
+        </div>
+        <div class="search">
+        <input type="text" name="" placeholder="Adresse Email">
+        <a> Rejoignez la communauté</a>
+        </div>
+       </div>
+</section>
+<section class="footer">
+<img src="../assets/logo.png">
+<div class="one">
+<ul class="items">
+
+<li><a href="">FAQ</a></li>
+<li><p>Des questions ? Appelez le 0805-220-512</p></li>
+<li><a href="">Relations Investisseurs</a></li>
+<li><a href="">Mentions légales</a></li>
+</ul>
+
+</div>
+<div class="one">
+<ul class="items">
+<li><a href="">Centre d'aide</a></li>
+<li><p>Des questions ? Appelez le 0805-220-512</p></li>
+<li><a href="">Conditions d'utilisation</a></li>
+<li><a href="">Nous contacter</a></li>
+</ul>
+
+</div>
+
+</section>
+  </div>
+  
 </template>
 <script>
+
   export default {
 
   }
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,10 +1,137 @@
 <template>
-  <div>
-      <p>Search</p>
+<div>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark indigo mb-4">
+
+  <!-- Navbar brand -->
+  <a class="navbar-brand" href="#">Navbar</a>
+
+  <!-- Collapsible content -->
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <form class="form-inline ml-auto">
+      <div class="md-form my-0">
+        <input class="form-control" type="text" placeholder="Search" aria-label="Search" v-model='query'  @keyup='getResult(query)'>
+      </div>
+      <button href="#!" class="btn btn-outline-white btn-md my-0 ml-sm-2" type="submit">Search</button>
+    </form>
+
   </div>
+  <!-- Collapsible content -->
+
+</nav>
+  <div >
+
+ <div class="d-flex justify-content-center">  <input class="form-control mr-sm-2" type="text"  v-model='query'  @keyup='getResult(query)' placeholder="Search" aria-label="Search">
+  </div>
+ 
+     <div class="movie-container">
+     
+      <div class="movie" v-for='result in results' :key='result.id'>
+      <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" class="img">
+       <div class="movie-info">
+       <h3>{{result.title}}</h3>
+        <span>{{result.vote_average}} </span>
+       </div>
+       <div class="movie-over">
+       <h2>Overwiew:</h2>
+           <p>{{result.overview}}</p>
+         </div>
+  
+      </div>
+     </div>
+  </div>
+</div>
 </template>
 <script>
-  export default {
-
+import axios from 'axios'
+export default {
+ name: 'search',
+ data () {
+  return {
+   query: '',
+   results: ''
   }
+ },
+ methods: {
+  getResult(query) {
+   axios.get('https://api.themoviedb.org/3/search/movie?api_key=1ac015c7464cb72bb59f1f1cc068a7c2&query=' + query).then(response => { this.results = response.data.results })
+     console.log(this.results)
+   }
+ }
+}
 </script>
+<style lang="scss" scoped>
+.movie-container{
+  display: flex;
+flex-wrap: wrap;
+}
+.movie{
+
+border-radius: 3px;
+box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1);
+overflow: hidden;
+position: relative;
+color: white; 
+margin: 1rem;
+width: 300px;
+}
+.movie-info h3{
+  margin: 0;
+}
+.movie-info{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+}
+.movie-over {
+    background-color: #fff;
+    border-top-left-radius:3px;
+    border-top-right-radius:3px;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1);
+    color: #22254b;
+    padding: 1rem;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow: auto;
+    max-height: 100%;
+    transform: translateX(100%);
+    transition: transform 0.3s ease-in-out;
+  }
+  .movie:hover .movie-over{
+    transform: translateX(0%);
+  }
+  .search{
+    background-color: transparent;
+border: 2px solid #22254b;
+border-radius: 50px;
+color: white;
+font-family: inherit;
+font-size:1.2rem;
+padding: 0.5rem 1.5rem;
+  }
+.input{
+  background-color: #373b69;
+display: flex;
+padding:1rem;
+justify-content:flex-end;
+}
+
+.img{
+  max-width : 100%;
+}
+.logo{
+  float: left;
+  display: inline-block;
+ width: 13%;
+  margin-right: 25px ;
+}
+.rightMenu{
+  float:right;
+  display: inline-block;
+  color:black;
+}
+</style>
